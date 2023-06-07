f60adbca62418bfe1fdb1f57622fe34c86d87e80
Christophe BAJAN <108456770+ChristopheBaj@users.noreply.github.com>
2023-06-07 15:50:59 +0900

    Update MADGUI.py

--- MADGUI/MADGUI.py
+++ MADGUI/MADGUI.py
@@ -10,10 +10,7 @@ import streamlit as st
 try:
     from streamlit.script_run_context import get_script_run_ctx
 except ModuleNotFoundError:
-    # streamlit < 1.4
-    from streamlit.report_thread import (  # type: ignore
-        get_report_ctx as get_script_run_ctx,
-    )          
+    from streamlit.report_thread import get_report_ctx as get_script_run_ctx,        
 from streamlit.server.server import Server    
 # Ref: https://gist.github.com/tvst/036da038ab3e999a64497f42de966a92
           
