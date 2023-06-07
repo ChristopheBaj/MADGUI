be9d3ce61fcd993b8501ac3bdbc8e5971aef7357
Christophe BAJAN <108456770+ChristopheBaj@users.noreply.github.com>
2023-06-07 15:58:50 +0900

    Update MADGUI.py

--- MADGUI/MADGUI.py
+++ MADGUI/MADGUI.py
@@ -6,15 +6,28 @@ import streamlit as st
 # from streamlit_option_menu import option_menu
 # import pandas as pd
 # import numpy as np
-
 try:
-    from streamlit.script_run_context import get_script_run_ctx
+    from streamlit.scriptrunner import get_script_run_ctx
+except ModuleNotFoundError:
+    # streamlit < 1.8
+    try:
+        from streamlit.script_run_context import get_script_run_ctx  # type: ignore
+    except ModuleNotFoundError:
+        # streamlit < 1.4
+        from streamlit.report_thread import (  # type: ignore
+            get_report_ctx as get_script_run_ctx,
+        )
 
-from streamlit.web.server.server import Server     
+from streamlit.server.server import Server
+
+# Ref: https://gist.github.com/tvst/036da038ab3e999a64497f42de966a92
+
+
 def get_session_id() -> str:
     ctx = get_script_run_ctx()
     if ctx is None:
-        raise Exception("Failed to get the thread context")    
+        raise Exception("Failed to get the thread context")
+
     return ctx.session_id
 
 if 'proof' not in st.session_state:
